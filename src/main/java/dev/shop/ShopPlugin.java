33ba9035d06a653b99bf1c8bd7e9c49a7e41b011
akiponakipon <ojiakaoldriver@gmail.com>
2025-07-30 18:34:05 +0900

    Update ShopPlugin.java

--- src/main/java/dev/shop/ShopPlugin.java
+++ src/main/java/dev/shop/ShopPlugin.java
@@ -1,19 +1,56 @@
-@Override
-public void onEnable() {
-    instance = this;
+package dev.shop;
 
-    this.shopManager = new ShopManager(this);
-    this.blacklistManager = new BlacklistManager(this);
-    this.economyManager = new EconomyManager(this);
+import dev.shop.commands.MoneyCommand;
+import dev.shop.commands.SellCommand;
+import dev.shop.commands.SellListCommand;
+import org.bukkit.plugin.java.JavaPlugin;
 
-    getServer().getPluginManager().registerEvents(new ShopListener(this), this);
+public class ShopPlugin extends JavaPlugin {
 
-    // SellCommand をインスタンス化して両方に渡す
-    SellCommand sellCommand = new SellCommand(this);
+    private static ShopPlugin instance;
+    private ShopManager shopManager;
+    private BlacklistManager blacklistManager;
+    private EconomyManager economyManager;
 
-    getCommand("sell").setExecutor(sellCommand);
-    getCommand("selllist").setExecutor(new SellListCommand(this, sellCommand));
-    getCommand("money").setExecutor(new MoneyCommand(this));
+    @Override
+    public void onEnable() {
+        instance = this;
 
-    getLogger().info("Shop Plugin Enabled!");
+        // マネージャー初期化
+        this.shopManager = new ShopManager(this);
+        this.blacklistManager = new BlacklistManager(this);
+        this.economyManager = new EconomyManager(this);
+
+        // イベント登録
+        getServer().getPluginManager().registerEvents(new ShopListener(this), this);
+
+        // コマンド登録
+        SellCommand sellCommand = new SellCommand(this);
+        getCommand("sell").setExecutor(sellCommand);
+        getCommand("selllist").setExecutor(new SellListCommand(this, sellCommand));
+        getCommand("money").setExecutor(new MoneyCommand(this));
+
+        getLogger().info("Shop Plugin Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Shop Plugin Disabled!");
+    }
+
+    public static ShopPlugin getInstance() {
+        return instance;
+    }
+
+    public ShopManager getShopManager() {
+        return shopManager;
+    }
+
+    public BlacklistManager getBlacklistManager() {
+        return blacklistManager;
+    }
+
+    public EconomyManager getEconomyManager() {
+        return economyManager;
+    }
 }
