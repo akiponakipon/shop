e2062f805439a15b6a7f93517ef6d5f5d1d37215
akiponakipon <ojiakaoldriver@gmail.com>
2025-07-30 18:16:51 +0900

    Update SellCommand.java

--- src/main/java/dev/shop/SellCommand.java
+++ src/main/java/dev/shop/SellCommand.java
@@ -8,12 +8,34 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SellCommand implements CommandExecutor {
 
     private final ShopPlugin plugin;
+    private final Map<Material, Double> sellPrices = new HashMap<>();
 
     public SellCommand(ShopPlugin plugin) {
         this.plugin = plugin;
+
+        // 売却リストを登録
+        sellPrices.put(Material.COPPER_INGOT, 0.002);
+        sellPrices.put(Material.COPPER_BLOCK, 0.018);
+        sellPrices.put(Material.COAL, 0.005);
+        sellPrices.put(Material.COAL_BLOCK, 0.045);
+        sellPrices.put(Material.LAPIS_LAZULI, 0.008);
+        sellPrices.put(Material.LAPIS_BLOCK, 0.072);
+        sellPrices.put(Material.IRON_INGOT, 0.015);
+        sellPrices.put(Material.IRON_BLOCK, 0.135);
+        sellPrices.put(Material.GOLD_INGOT, 5.0);
+        sellPrices.put(Material.GOLD_BLOCK, 45.0);
+        sellPrices.put(Material.DIAMOND, 15.0);
+        sellPrices.put(Material.DIAMOND_BLOCK, 135.0);
+        sellPrices.put(Material.EMERALD, 27.5);
+        sellPrices.put(Material.EMERALD_BLOCK, 247.5);
+        sellPrices.put(Material.NETHERITE_INGOT, 125.0);
+        sellPrices.put(Material.NETHERITE_BLOCK, 1125.0);
     }
 
     @Override
@@ -30,16 +52,22 @@ public class SellCommand implements CommandExecutor {
             return true;
         }
 
-        // 例: 石炭を 0.005 Yen で売る
-        if (item.getType() == Material.COAL) {
+        Material type = item.getType();
+        if (sellPrices.containsKey(type)) {
             int amount = item.getAmount();
-            double price = 0.005 * amount;
+            double price = sellPrices.get(type) * amount;
+
             plugin.getEconomyManager().addMoney(p, price);
             p.getInventory().setItemInMainHand(null);
-            p.sendMessage("§a" + amount + "個の石炭を " + price + " Yen で売却しました！");
+
+            p.sendMessage("§a" + amount + "個の " + type.name() + " を " + price + " Yen で売却しました！");
         } else {
             p.sendMessage("§eこのアイテムは売却できません");
         }
         return true;
+    }
+
+    public Map<Material, Double> getSellPrices() {
+        return sellPrices;
     }
 }
